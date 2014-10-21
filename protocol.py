a9aba0ba5719e4059397d81fab418a511f852c1f
Yorickdewid <yorick17@outlook.com>
2014-10-21 11:16:41 -0400

    User registration

--- protocol.py
+++ protocol.py
@@ -7,13 +7,41 @@ import json
 
 class Echo(Protocol):
     
-    def clientPing(self):
+    def clientPing(self, data):
         message = [{'error':'false', 'code': 206, 'message':'Pong'}]
         self.transport.write(json.dumps(message) + '\n')
+
+    def clientRegister(self, data):
+        try:
+            username = data['username']
+            password = data['password']
+            token = data['token']
+
+            if len(password) != 8:
+                message = [{'error':'true', 'code': 405, 'message':'Password invalid'}]
+                self.transport.write(json.dumps(message) + '\n')
+                return
+
+            if len(token) != 40:
+                message = [{'error':'true', 'code': 406, 'message':'Token invalid'}]
+                self.transport.write(json.dumps(message) + '\n')
+                return
+
+            user = User(username)
+            user.token = token
+            user.password = password
+            user.save()
+
+            message = [{'error':'false', 'code': 241, 'message':'User registered'}]
+            self.transport.write(json.dumps(message) + '\n')
+        except KeyError:
+            message = [{'error':'true', 'code': 401, 'message':'Not in reference format'}]
+            self.transport.write(json.dumps(message) + '\n')
 
     def handle(self, x):
         return {
             205 : self.clientPing,
+            240 : self.clientRegister,
         }[x]
 
     def __init__(self):
@@ -29,8 +57,10 @@ class Echo(Protocol):
             self.error = req['error']
             self.message = req['message']
 
-            print self.code
-            self.handle(self.code)()
+            if 'data' in req:
+                self.data = req['data']
+
+            self.handle(self.code)(self.data)
         except ValueError:
             message = [{'error':'true', 'code': 400, 'message':'Send data in JSON'}]
             self.transport.write(json.dumps(message) + '\n')
